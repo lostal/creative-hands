f854ad7bf57e911a52d66c2d2c6a3e42e2333855
lostal <alvarolostal04@gmail.com>
2025-12-11 17:22:17 +0100

    reinicio

--- client/src/components/admin/index.js
+++
@@ -1,4 +0,0 @@
-// Componentes del panel de administración
-export { default as ProductFormModal } from "./ProductFormModal";
-export { default as CategoryModal } from "./CategoryModal";
-export { default as CategoryEditModal } from "./CategoryEditModal";
--- client/src/components/home/index.js
+++
@@ -1,5 +0,0 @@
-// Home Page Components - v2 Design System
-export { default as StatsBar } from './StatsBar';
-export { default as CategoryGrid } from './CategoryGrid';
-export { default as ProcessTimeline } from './ProcessTimeline';
-export { default as TestimonialSection } from './TestimonialSection';
